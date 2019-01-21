d6763dcd3708e688d020a85ae1a610701ae868b7
Faris Habib <faris.habib.7@gmail.com>
2019-01-21 13:11:57 -0800

    Round VIII

--- knexfile.js
+++ knexfile.js
@@ -10,5 +10,17 @@ module.exports = {
     seeds: {
       directory: './database/seeds'
     }
+  },
+  production: {
+    client: 'pg',
+    connection: {
+      host: process.env.RDS_HOSTNAME || '127.0.0.1',
+      user: process.env.RDS_USERNAME || 'taco',
+      password: process.env.RDS_PASSWORD || 'tuesday',
+      database: process.env.RDS_DB_NAME || 'twitchvid'
+    },
+    seeds: {
+      directory: './database/seeds'
+    }
   }
 };
