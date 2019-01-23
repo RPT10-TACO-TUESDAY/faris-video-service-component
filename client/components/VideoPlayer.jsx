02cf70c7a327ae3814774676169d218bb465472e
Faris Habib <faris.habib.7@gmail.com>
2019-01-22 19:59:49 -0800

    Added additional styling changes to make sure the video player description stretches with the video

--- client/components/VideoPlayer.jsx
+++ client/components/VideoPlayer.jsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 
 const VideoPlayerStyle = styled.div`
   display: inline-block;
-  width: 1081px;
+  width: 100%;
   box-sizing: content-box;
   borderStyle: solid;
   borderWidth: 1px;
